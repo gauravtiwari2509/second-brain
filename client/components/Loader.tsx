2465fcaea3e1c0d25e8dcc81e7cbb9e5e254a006
gauravtiwari2509 <gauravtiwari2509@gmail.com>
2025-01-12 22:29:12 +0530

    updated loader

--- client/components/Loader.tsx
+++ client/components/Loader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => (
   <div className="fixed h-screen w-screen top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-gray-500 bg-opacity-50 z-50">
-    <div className="spinner-border animate-spin border-4 border-t-4 border-blue-500 w-16 h-16 rounded-full"></div>
+    <span className="loader"></span>
   </div>
 );
 
